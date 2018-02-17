2b7cff54170f2b85feac8200b435ad3d76abd78c
Sille Kamoen <sille.kamoen@gmail.com>
2018-02-17 12:55:13 +0100

    Remove unused menu and add instructions

--- src/components/configuration-overview.js
+++ src/components/configuration-overview.js
@@ -6,29 +6,48 @@ import { Link } from 'react-router';
 import View from './view';
 
 class ConfigurationOverview extends Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	componentWillMount() {
-		const { dispatch } = this.props;
-	}
+  componentWillMount() {
+    const { dispatch } = this.props;
+  }
 
-	renderTable() {
-	}
+  renderTable() {}
 
-	render() {
-		return (
-			<View title="Overview" subtitle="Configuration">
-            Configuration
-			</View>
-		);
-	}
+  render() {
+    return ( 
+	<View title = "Overview" subtitle = "Configuration" >
+      <p>Honeytrack Agent</p>
+
+		<p>The Honeytrack agent is a small lightweight honeypot listener that forwards traffic to a central honeytrack server.</p>
+
+		<p>Installation
+		The Honeytrack Agent is still experimental software. It's stable for general usage, but you may need to restart or update. 
+		On startup, it connects to the central server to receive its configuration.
+		At this time, the agents are instructed to listen to port 23 (Telnet). You should forward port 23 to your device in your router.</p>
+
+		<p>There are a few installation options.
+		The easiest option is the provided zip file. This contains an executable, configuration file and start script.
+		You should run this in a <code>screen</code> session, or run the start script as <code>nohup sudo ./startAgent.sh &amp;</code></p>
+
+		<p>Alternatively, you can build the agent from source. For this, you need Go(lang) installed, at least version 1.8.
+		Once Go is installed, get the package with <code>go get github.com/honeytrap/honeytrap-agent</code>, <code>cd $GOPATH/src/github.com/honeytrap/honeytrap-agent</code>
+		finally build with <code>go build -o bin/honeytrap-agent</code>. Create a configuration file with the following content:</p>
+
+		<p>server="honeytrack.cyber-threat-intelligence.com:12000"
+		remote-key="a842e2a34911b311e40892da583185fb3ba0748f1931bfcc53f6e5e16898ff06"</p>
+
+		<p>You can then run the agent with <code>sudo ./honeytrap-agent -f config.toml</code>. Note that the agent runs in the foreground.
+		You need to run it in a <code>screen</code> session or use something like <code>nohup</code> to run it in the background</p> 
+	</View>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-	};
+  return {};
 }
 
 export default connect(mapStateToProps)(ConfigurationOverview);
